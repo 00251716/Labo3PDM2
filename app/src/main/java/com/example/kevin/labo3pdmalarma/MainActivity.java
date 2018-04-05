80499d0ab85a35a195993c9e33e512aa24060b6a
kevin <0010216@uca.edu.sv>
2018-04-04 23:30:38 -0600

    Se agregó funcionalidad a los CheckBox. El ArrayList se limpia después de enviar los datos.

--- app/src/main/java/com/example/kevin/labo3pdmalarma/MainActivity.java
+++ app/src/main/java/com/example/kevin/labo3pdmalarma/MainActivity.java
@@ -12,27 +12,90 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     Boolean switchValue;
+    ArrayList<Integer> diasAlarma = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox checkBoxLunes = (CheckBox) findViewById(R.id.L);
-        CheckBox checkBoxMartes = (CheckBox) findViewById(R.id.M);
-        CheckBox checkBoxMiercoles = (CheckBox) findViewById(R.id.X);
-        CheckBox checkBoxJueves = (CheckBox) findViewById(R.id.J);
-        CheckBox checkBoxViernes = (CheckBox) findViewById(R.id.V);
-        CheckBox checkBoxSabado = (CheckBox) findViewById(R.id.S);
-        CheckBox checkBoxDomingo = (CheckBox) findViewById(R.id.D);
+        //Obteniendo los widgets del heap
+
+        final CheckBox checkBoxLunes = (CheckBox) findViewById(R.id.L);
+        final CheckBox checkBoxMartes = (CheckBox) findViewById(R.id.M);
+        final CheckBox checkBoxMiercoles = (CheckBox) findViewById(R.id.X);
+        final CheckBox checkBoxJueves = (CheckBox) findViewById(R.id.J);
+        final CheckBox checkBoxViernes = (CheckBox) findViewById(R.id.V);
+        final CheckBox checkBoxSabado = (CheckBox) findViewById(R.id.S);
+        final CheckBox checkBoxDomingo = (CheckBox) findViewById(R.id.D);
         final Switch miSwitch = (Switch) findViewById(R.id.miSwitch);
         Button buttonOk = (Button) findViewById(R.id.buttonOk) ;
         final EditText editText = (EditText) findViewById(R.id.editText);
         final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
 
+
+        //seteando las acciones apropiadads
+
+        checkBoxLunes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxLunes.isChecked()) diasAlarma.add(Calendar.MONDAY);
+            }
+        });
+
+
+        checkBoxMartes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxMartes.isChecked()) diasAlarma.add(Calendar.TUESDAY);
+            }
+        });
+
+
+        checkBoxMiercoles.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxMiercoles.isChecked()) diasAlarma.add(Calendar.WEDNESDAY);
+            }
+        });
+
+
+        checkBoxJueves.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxJueves.isChecked()) diasAlarma.add(Calendar.THURSDAY);
+            }
+        });
+
+
+        checkBoxViernes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxViernes.isChecked()) diasAlarma.add(Calendar.FRIDAY);
+            }
+        });
+
+
+        checkBoxSabado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxSabado.isChecked()) diasAlarma.add(Calendar.SATURDAY);
+            }
+        });
+
+
+        checkBoxDomingo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBoxDomingo.isChecked()) diasAlarma.add(Calendar.SUNDAY);
+            }
+        });
 
         miSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -41,6 +104,8 @@ public class MainActivity extends AppCompatActivity {
                 else switchValue = false;
             }
         });
+
+        //Aqui sucede la magia del intent
 
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,10 +116,12 @@ public class MainActivity extends AppCompatActivity {
                         .putExtra(AlarmClock.EXTRA_MESSAGE, editText.getText().toString())
                         .putExtra(AlarmClock.EXTRA_HOUR, timePicker.getCurrentHour())
                         .putExtra(AlarmClock.EXTRA_MINUTES, timePicker.getCurrentMinute())
-                        .putExtra(AlarmClock.EXTRA_VIBRATE, switchValue);
+                        .putExtra(AlarmClock.EXTRA_VIBRATE, switchValue)
+                        .putExtra(AlarmClock.EXTRA_DAYS, diasAlarma);
 
                 if(intentAlarma.resolveActivity(getPackageManager()) != null) startActivity(intentAlarma);
 
+                diasAlarma.clear(); //Hay que limpiar el arreglo, si no hay problemas
             }
         });
 
